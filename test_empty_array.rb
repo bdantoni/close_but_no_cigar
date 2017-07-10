8118fb4b147bff9c83adfc7a3da64f5864f5903e
Bailey DAntoni <bkdmarshall@hotmail.com>
2017-07-10 14:49:17 -0400

    adding to the classwork with array testing.

--- test_empty_array.rb
+++ test_empty_array.rb
@@ -1,4 +1,21 @@
-def array
-	a = []
-end
+# def array(yourticket)
+# 	a = []
 
+# 	yourticket = "5678"
+
+
+# end
+
+def array(your_ticket, winning_ticket)
+
+	matches = []
+	winning_ticket.each do |winning_ticket|
+		if your_ticket == winning_ticket
+			matches << your_ticket
+			p "you win"
+		# else your_ticket != winning_ticket
+		# 	p "you lose"
+		end
+end
+matches
+end
--- testing_the_array.rb
+++ testing_the_array.rb
@@ -2,17 +2,15 @@ require "minitest/autorun"
 require_relative "test_empty_array.rb"
 
 class Empty_array < Minitest::Test
-	def test_empty_array
-		p array
-		assert_equal(Array,array.class)
+	# def test_empty_array
+	# 	p array
+	# 	assert_equal(Array,array.class)
+	# end
+
+	def test_winning_numbers
+		your_ticket = "5678"
+		winning_numbers = ["5678", "9869", "9948"]
+		assert_equal(["5678"],array(your_ticket,winning_numbers))
 	end
-
-
-
-
-
-
-
-
 
 end
