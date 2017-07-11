a2df27e70fe339530fe62debd4f99ef9ea5a3794
Bailey DAntoni <bkdmarshall@hotmail.com>
2017-07-11 11:46:30 -0400

    working on testing strings of numbers.

--- testing_the_array.rb
+++ testing_the_array.rb
@@ -3,14 +3,31 @@ require_relative "test_empty_array.rb"
 
 class Empty_array < Minitest::Test
 	# def test_empty_array
-	# 	p array
-	# 	assert_equal(Array,array.class)
+	#  	p array
+	#  	assert_equal(Array,array.class)
 	# end
 
-	def test_winning_numbers
-		your_ticket = "5678"
-		winning_numbers = ["5678", "9869", "9948"]
-		assert_equal(["5678"],array(your_ticket,winning_numbers))
-	end
+	# def test_winning_numbers
+	#  	your_ticket = "5678"
+	#  	winning_numbers = ["5678", "9869", "9948"]
+	#  	assert_equal(["5678"],array(your_ticket,winning_numbers))
+	# end
 
+	# def test_losing_numbers
+	# 	your_ticket = "7685"
+	#  	winning_numbers = ["5678", "9869", "9948"]
+	#  	assert_equal(["7685"],array(your_ticket,winning_numbers))
+	# end
+
+	# def test_off_one_number
+	# 	your_ticket = "5679"
+	# 	winning_numbers = ["5678", "9869", "9948"]
+	# 	assert_equal(["5679"],array(your_ticket,winning_numbers))
+	# end
+
+	def test_off_one_number
+		winning_ticket =["5679","9489","6372"]
+ 		losing_ticket ="5679"
+		assert_equal(2,array_1_number_off(winning_ticket,losing_ticket))
+	end
 end
