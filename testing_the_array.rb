e25014ca82f3f68e7a6a1448fb3023678b31e359
Bailey DAntoni <bkdmarshall@hotmail.com>
2017-07-11 12:17:11 -0400

    testing one string.

--- testing_the_array.rb
+++ testing_the_array.rb
@@ -26,8 +26,11 @@ class Empty_array < Minitest::Test
 	# end
 
 	def test_off_one_number
-		winning_ticket =["5679","9489","6372"]
- 		losing_ticket ="5679"
-		assert_equal(2,array_1_number_off(winning_ticket,losing_ticket))
+		winning_ticket ="5679"
+ 		losing_ticket ="5671"
+		# assert_equal(4,array_1_number_off(winning_ticket,losing_ticket))
+		assert_equal(true,array_1_number_off(winning_ticket,losing_ticket))
 	end
+
+
 end
