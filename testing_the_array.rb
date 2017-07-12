935dc81c20bad9290b14e507302dfb9b91633394
Bailey DAntoni <bkdmarshall@hotmail.com>
2017-07-12 11:38:36 -0400

    comparing a string with an array and testing it.

--- testing_the_array.rb
+++ testing_the_array.rb
@@ -2,28 +2,6 @@ require "minitest/autorun"
 require_relative "test_empty_array.rb"
 
 class Empty_array < Minitest::Test
-	# def test_empty_array
-	#  	p array
-	#  	assert_equal(Array,array.class)
-	# end
-
-	# def test_winning_numbers
-	#  	your_ticket = "5678"
-	#  	winning_numbers = ["5678", "9869", "9948"]
-	#  	assert_equal(["5678"],array(your_ticket,winning_numbers))
-	# end
-
-	# def test_losing_numbers
-	# 	your_ticket = "7685"
-	#  	winning_numbers = ["5678", "9869", "9948"]
-	#  	assert_equal(["7685"],array(your_ticket,winning_numbers))
-	# end
-
-	# def test_off_one_number
-	# 	your_ticket = "5679"
-	# 	winning_numbers = ["5678", "9869", "9948"]
-	# 	assert_equal(["5679"],array(your_ticket,winning_numbers))
-	# end
 
 	def test_off_one_number
 		winning_ticket ="5679"
@@ -31,11 +9,36 @@ class Empty_array < Minitest::Test
 		# assert_equal(4,array_1_number_off(winning_ticket,losing_ticket))
 		assert_equal(true,array_1_number_off(winning_ticket,losing_ticket))
 	end
-
-	def test_off_1_arrays
-		winning_ticket2 = ["1234","3452","7483"]
-	 	losing_ticket2 = "1237"
-		assert_equal(true,array_3_strings(winning_ticket2,losing_ticket2))
+	
+	def test_same_number
+		winning_ticket ="5679"
+ 		losing_ticket ="5679"
+		assert_equal(false,array_1_number_off(winning_ticket,losing_ticket))
 	end
 
-end
+	def test_no_match_empty_array
+	 	winning_ticket2 = ["1234","3452","7483"]
+	  	losing_ticket2 = "1237"
+	 	assert_equal(["1234"],winning_array(winning_ticket2,losing_ticket2))
+	 end
+
+	 def test_exact_match
+	 	winning_ticket2 = ["1234"]
+	 	losing_ticket2 = "1234"
+	 	assert_equal([],winning_array(winning_ticket2,losing_ticket2))
+	 end
+
+	 def test_exact_match_2
+	 	winning_ticket2 = ["1234","1239","4738","7392"]
+	 	losing_ticket2 = "1234"
+	 	assert_equal(["1239"],winning_array(winning_ticket2,losing_ticket2))
+	 end
+
+	 def test_exact_match_3
+	 	winning_ticket2 = ["1236","1254","4542","7960","5849","1259"]
+	 	losing_ticket2 = "1234"
+	 	assert_equal(["1236","1254"],winning_array(winning_ticket2,losing_ticket2))
+	 end
+
+
+ end
